7ac82614c12c2d9e06d62f6765541bd20c0d8be1
Your Name <mash.pogonina2014@yandex.ru>
2023-05-10 23:29:51 +0300

    tests

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', require('./routes/index'));
 
 app.use(errorLogger);
